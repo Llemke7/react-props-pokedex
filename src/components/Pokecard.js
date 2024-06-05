2c03f51d2fd64e8862b7a61e0010f17d2fc9b50f
Luke Lemke <llemke777@gmail.com>
2024-06-05 14:00:21 -0500

    second commit

---
+++ src/components/Pokecard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Pokecard({ id, name, type, base_experience }) {
+  const imgSrc = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+  
+  return (
+    <div className="Pokecard">
+      <h1 className="Pokecard-title">{name}</h1>
+      <img className="Pokecard-image" src={imgSrc} alt={name} />
+      <div className="Pokecard-data">Type: {type}</div>
+      <div className="Pokecard-data">EXP: {base_experience}</div>
+    </div>
+  );
+}
+
+export default Pokecard;
